c7bbb40b92e3405dfb39ebc8de1c0ae89837ed21
jessica <jessicalh@hotmail.com>
2026-06-11 00:29:04 +0100

    baseline: track C++ reader source only (cpp/h+build); ignore R/Python/analysis/npy cruft

--- nmrstats/R/deps.R
+++
@@ -1,9 +0,0 @@
-dependency_anchor <- function() {
-  # These packages are part of the declared statistical stack. Some real-pass
-  # adjustments use faster shrinkage/bam paths, but keeping the namespaces
-  # anchored makes DESCRIPTION honest and check-clean.
-  invisible(list(
-    lme4 = lme4::lmerControl,
-    nlme = nlme::lmeControl
-  ))
-}
